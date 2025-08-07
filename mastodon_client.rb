1c4582b83a74366c56c277f5b5848877d2b6e3b2
CriminalTalent <rntjdrud@naver.com>
2025-08-07 12:52:06 +0900

    Update mastodon_client.rb

--- mastodon_client.rb
+++ mastodon_client.rb
@@ -9,24 +9,16 @@ class MastodonClient
       base_url: ENV['MASTODON_BASE_URL'],
       bearer_token: ENV['MASTODON_ACCESS_TOKEN']
     )
-    @account = @client.verify_credentials
-  end
-
-  def reply(to_user, text, in_reply_to_id: nil)
-    status = "@#{to_user} #{text}"
-    @client.create_status(status, in_reply_to_id: in_reply_to_id)
+    @me = @client.verify_credentials
   end
 
   def say(text)
     @client.create_status(text)
   end
 
-  def boost(status_id)
-    @client.reblog(status_id)
+  def reply(user_id, text, in_reply_to_id: nil)
+    @client.create_status("@#{user_id} #{text}", in_reply_to_id: in_reply_to_id)
   end
 
-  def account_id
-    @account.acct
-  end
-end
-
+  def me
+    @me.
