52bed50c7ae038dc8f992eba755c0779c37fce8a
CriminalTalent <rntjdrud@naver.com>
2025-08-07 12:55:24 +0900

    Update battle_command.rb

--- commands/battle_command.rb
+++ commands/battle_command.rb
@@ -5,8 +5,8 @@ require_relative '../core/sheet_manager'
 require_relative '../core/battle_state'
 
 class BattleCommand
-  def initialize(masto_client, sheet)
-    @client = masto_client
+  def initialize(masto, sheet)
+    @masto = masto
     @sheet = sheet
   end
 
@@ -14,16 +14,17 @@ class BattleCommand
     content = status[:content]
     user_id = status[:account][:acct]
     display_name = status[:account][:display_name] || user_id
+    in_reply_to_id = status[:id]
 
     case content
     when /^전투개시\s+@(\w+)/
       opponent = "@#{$1}"
-      start_battle(user_id, opponent)
+      start_battle(user_id, opponent, in_reply_to_id)
 
     when /^DM전투개시\s+(.+)vs(.+)/
       team_a = $1.strip.split(/\s+/)
       team_b = $2.strip.split(/\s+/)
-      start_dm_battle(team_a, team_b)
+      start_dm_battle(team_a, team_b, in_reply_to_id)
 
     when /공격/
       BattleEngine.attack(user_id)
@@ -41,16 +42,15 @@ class BattleCommand
       BattleEngine.use_potion(user_id)
 
     else
-      # 무시
       return
     end
   end
 
   private
 
-  def start_battle(user_id, opponent_id)
+  def start_battle(user_id, opponent_id, reply_id)
     if BattleState.in_battle?(user_id) || BattleState.in_battle?(opponent_id)
-      @client.reply("@#{user_id} 혹은 #{opponent_id}는 이미 전투 중입니다.")
+      @masto.reply(user_id, "당신 혹은 #{opponent_id}는 이미 전투 중입니다.", in_reply_to_id: reply_id)
       return
     end
 
@@ -59,10 +59,10 @@ class BattleCommand
     BattleEngine.roll_initiative(players)
   end
 
-  def start_dm_battle(team_a, team_b)
+  def start_dm_battle(team_a, team_b, reply_id)
     players = team_a + team_b
     if players.any? { |p| BattleState.in_battle?(p) }
-      @client.reply("참가자 중 이미 전투 중인 유저가 있습니다.")
+      @masto.say("참가자 중 이미 전투 중인 유저가 있습니다.")
       return
     end
 
@@ -70,4 +70,3 @@ class BattleCommand
     BattleEngine.roll_team_initiative(team_a, team_b)
   end
 end
-
