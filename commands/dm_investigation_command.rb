75a2985052a64fbd7bdff2ac844c3c4b87521d4e
CriminalTalent <rntjdrud@naver.com>
2025-08-10 21:20:15 +0900

    Update dm_investigation_command.rb

--- commands/dm_investigation_command.rb
+++ commands/dm_investigation_command.rb
@@ -1,24 +1,25 @@
 # commands/dm_investigation_command.rb
 require 'date'
-require_relative '../core/sheet_manager'
 
 class DMInvestigationCommand
-  def initialize(masto)
-    @masto = masto
+  def initialize(mastodon_client, sheet_manager)
+    @mastodon_client = mastodon_client
+    @sheet_manager = sheet_manager
   end
 
   def handle(status)
-    content = status.content.gsub(/<[^>]+>/, '')
+    content = status.content.gsub(/<[^>]+>/, '').strip
     return unless content.start_with?("DM조사결과")
 
-    match = content.match(/DM조사결과\s+@(\w+)\s+(.+)/)
+    match = content.match(/DM조사결과\s+@?(\w+)\s+(.+)/)
     return unless match
 
     user = match[1]
     result = match[2]
     today = Date.today.to_s
 
-    SheetManager.set_stat(user, "마지막조사일", today)
-    @masto.dm(user, result)
+    @sheet_manager.set_stat(user, "마지막조사일", today)
+    @mastodon_client.dm(user, result)
+    puts "[DM조사] #{user}에게 결과 전송 완료"
   end
 end
