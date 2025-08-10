d7052efd23c14f336eefcb55c863a8c65a081a3a
CriminalTalent <rntjdrud@naver.com>
2025-08-10 21:21:07 +0900

    Update potion_command.rb

--- commands/potion_command.rb
+++ commands/potion_command.rb
@@ -1,40 +1,44 @@
 # commands/potion_command.rb
-require_relative '../core/sheet_manager'
 
 class PotionCommand
-  def initialize(masto, sheet)
-    @masto = masto
-    @sheet = sheet
+  def initialize(mastodon_client, sheet_manager)
+    @mastodon_client = mastodon_client
+    @sheet_manager = sheet_manager
   end
 
   def handle(status)
-    user_id = status.account.acct
-    inventory = SheetManager.get_stat(user_id, "아이템")
+    sender_full = status.account.acct
+    sender = sender_full.split('@').first
+    in_reply_to_id = status.id
+    
+    inventory = @sheet_manager.get_stat(sender, "아이템")
     return unless inventory
 
-    potion_line = inventory.split(',').find { |i| i.include?("포션") }
+    potion_line = inventory.split(',').find { |i| i.include?("물약") }
     unless potion_line
-      @masto.say("@#{user_id}는 포션을 가지고 있지 않습니다.")
+      @mastodon_client.reply(sender, "물약을 가지고 있지 않습니다.", in_reply_to_id: in_reply_to_id)
       return
     end
 
-    count = potion_line.match(/포션(\d+)/)&.[](1)&.to_i || 0
+    count = potion_line.match(/물약:?(\d+)/)&.[](1)&.to_i || 0
     if count <= 0
-      @masto.say("@#{user_id}는 포션이 없습니다.")
+      @mastodon_client.reply(sender, "물약이 없습니다.", in_reply_to_id: in_reply_to_id)
       return
     end
 
     # 랜덤 회복량
     amount = [5, 10, 15, 20].sample
-    hp = SheetManager.get_stat(user_id, "체력").to_i
+    hp_str = @sheet_manager.get_stat(sender, "체력")
+    hp = hp_str ? hp_str.to_i : 100
     new_hp = [hp + amount, 100].min
 
-    SheetManager.set_stat(user_id, "체력", new_hp)
+    @sheet_manager.set_stat(sender, "체력", new_hp)
 
     # 포션 차감
-    new_inventory = inventory.gsub(/포션:\d+/) { "포션:#{count - 1}" }
-    SheetManager.set_stat(user_id, "아이템", new_inventory)
+    new_inventory = inventory.gsub(/물약:?\d*/) { "물약:#{count - 1}" }
+    @sheet_manager.set_stat(sender, "아이템", new_inventory)
 
-    @masto.say("@#{user_id}의 체력이 #{amount} 회복되었습니다. 현재 체력 #{new_hp}")
+    message = "#{sender}의 체력이 #{amount} 회복되었습니다. 현재 체력 #{new_hp}"
+    @mastodon_client.say(message)
   end
 end
