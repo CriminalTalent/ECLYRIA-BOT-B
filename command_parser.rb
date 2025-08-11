440944eb53c7e81f232116b16c786afbc055433f
CriminalTalent <rntjdrud@naver.com>
2025-08-11 13:05:46 +0900

    Update command_parser.rb

--- command_parser.rb
+++ command_parser.rb
@@ -2,6 +2,7 @@
 require_relative 'commands/battle_command'
 require_relative 'commands/potion_command'
 require_relative 'commands/investigate_command'
+require_relative 'commands/enhanced_investigate_command'
 require_relative 'commands/dm_investigation_command'
 
 class CommandParser
@@ -13,6 +14,7 @@ class CommandParser
     @battle = BattleCommand.new(@mastodon_client, @sheet_manager)
     @potion = PotionCommand.new(@mastodon_client, @sheet_manager)
     @investigate = InvestigateCommand.new(@mastodon_client, @sheet_manager)
+    @enhanced_investigate = EnhancedInvestigateCommand.new(@mastodon_client, @sheet_manager)
     @dm = DMInvestigationCommand.new(@mastodon_client, @sheet_manager)
   end
 
@@ -27,14 +29,18 @@ class CommandParser
     
     # 명령어 라우팅
     case content
-    when /전투개시|DM전투개시|공격|방어|반격|도주/
+    when /전투개시|허수아비|공격|방어|반격|도주/
       @battle.handle(status)
     when /물약사용/
       @potion.handle(status)
-    when /조사|정밀조사|감지|훔쳐보기/
-      @investigate.handle(status)
     when /DM조사결과/
       @dm.handle(status)
+    when /이동|위치확인|주변탐색|은신|협력조사|방해|물건이동|숨기기|흔적조사|조사기록|타임라인/
+      # 고도화된 조사 시스템 명령어들
+      @enhanced_investigate.handle(status)
+    when /조사|정밀조사|감지|훔쳐보기/
+      # 기존 조사 명령어는 고도화된 시스템으로 처리
+      @enhanced_investigate.handle(status)
     else
       puts "[무시] 인식되지 않은 명령어: #{content}"
     end
