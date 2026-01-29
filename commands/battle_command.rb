4c8436f8466d6470d74fb2e55c77b808cfdf74ff
CriminalTalent <rntjdrud@naver.com>
2026-01-29 22:53:00 +0900

    Refactor battle command methods for clarity

--- commands/battle_command.rb
+++ commands/battle_command.rb
@@ -8,43 +8,35 @@ class BattleCommand
     @engine          = BattleEngine.new(mastodon_client, sheet_manager)
   end
 
-  def handle_command(user_id, text, reply_status)
-    puts "[BattleCommand] handle_command: #{text} from #{user_id}"
+  def start_1v1(user1_id, user2_id, reply_status)
+    @engine.start_1v1(user1_id, user2_id, reply_status)
+  end
 
-    sanitize = ->(s) { s.to_s.gsub(/\p{Cf}/, '').strip.sub(/\A@+/, '') }
+  def start_2v2(user1_id, user2_id, user3_id, user4_id, reply_status)
+    @engine.start_2v2(user1_id, user2_id, user3_id, user4_id, reply_status)
+  end
 
-    case text
-    when /\A\[전투\s+@?(\S+)\s+vs\s+@?(\S+)\]\z/i
-      raw1, raw2 = $1, $2
-      u1 = sanitize.call(raw1)
-      u2 = sanitize.call(raw2)
-      @engine.start_1v1(u1, u2, reply_status) unless u1.empty? || u2.empty?
+  def start_4v4(u1, u2, u3, u4, u5, u6, u7, u8, reply_status)
+    @engine.start_4v4(u1, u2, u3, u4, u5, u6, u7, u8, reply_status)
+  end
 
-    when /\A\[다인전투\/@?(\S+)\/@?(\S+)\/@?(\S+)\/@?(\S+)\]\z/i
-      a, b, c, d = $1, $2, $3, $4
-      u1, u2, u3, u4 = [a, b, c, d].map { |x| sanitize.call(x) }
-      @engine.start_2v2(u1, u2, u3, u4, reply_status) if [u1, u2, u3, u4].none?(&:empty?)
+  def attack(user_id, target_id, reply_status)
+    @engine.attack(user_id, target_id)
+  end
 
-    when /\[공격\/@?(\S+)\]/i
-      @engine.attack(user_id, sanitize.call($1))
-
-    when /\[공격\]/i
-      @engine.attack(user_id)
-
-    when /\[방어\/@?(\S+)\]/i
-      @engine.defend_target(user_id, sanitize.call($1))
-
-    when /\[방어\]/i
+  def defend(user_id, target_id, reply_status)
+    if target_id
+      @engine.defend_target(user_id, target_id)
+    else
       @engine.defend(user_id)
-
-    when /\[반격\]/i
-      @engine.counter(user_id)
-
-    when /\[도주\]/i
-      @engine.flee(user_id)
-
-    when /\[허수아비\s*(하|중|상)\]/i
-      @engine.start_dummy_battle(user_id, $1, reply_status)
     end
+  end
+
+  def counter(user_id, reply_status)
+    @engine.counter(user_id)
+  end
+
+  def flee(user_id, reply_status)
+    @engine.flee(user_id)
   end
 end
